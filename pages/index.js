2036e351ed95b1664f4dc01823f0a00743528eb8
DominicJoseph98 <mohanjose1998@gmail.com>
2025-06-26 22:02:53 +0530

    Upload bridgelearn website

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'Arial', textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to BridgeLearn</h1>
+      <p>Connecting students from underprivileged backgrounds with volunteers for learning support.</p>
+    </main>
+  )
+}
