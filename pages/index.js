6d3a851e65ff28d9bc2cd5bfa05ea847a00747e3
DominicJoseph98 <mohanjose1998@gmail.com>
2025-06-26 22:05:04 +0530

    Remove pages/ to resolve conflict.

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ fontFamily: 'Arial', textAlign: 'center', padding: '2rem' }}>
-      <h1>Welcome to BridgeLearn</h1>
-      <p>Connecting students from underprivileged backgrounds with volunteers for learning support.</p>
-    </main>
-  )
-}
